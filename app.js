45e0afee1c54070a62378ffef46723468c99eeb5
Kham Laung <shwegyimontkyut@gmail.com>
2022-06-25 08:43:09 +0630

    Fix Mongo URI

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://express-library-app:node-app@cluster0.6ug2m.mongodb.net/?retryWrites=true'
-//var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+//var dev_db_url = 'mongodb+srv://express-library-app:node-app@cluster0.6ug2m.mongodb.net/?retryWrites=true'
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
